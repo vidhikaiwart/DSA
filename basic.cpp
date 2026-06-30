ca9022bfbfd959bbfb0bbd1368deed30ffa0e17d
vidhikaiwart <vidhi.laymotion01@gmail.com>
2026-06-30 22:34:29 +0530

    3.Check if a number is Palindrome.

--- basic.cpp
+++ basic.cpp
@@ -33,32 +33,72 @@
 
 
 
-//2 Print all Prime numbers in a range.
+// //2 Print all Prime numbers in a range.
+// #include <iostream>
+// #include <string>
+// using namespace std; 
+
+// void primeNumber(int s, int e ){
+//  for(int i = s; i<=e; i++){
+//     int j = 2;
+//   if(i==1) cout << "1 is neither prime nor composite : "<< i <<endl;
+//   if(i==2) cout << "2 is prime number : " << i <<endl;
+//   else{
+//     while(j<i){
+//         if(i%j==0){
+//             cout<<i<<" is not a prime number :  " << i <<endl;
+//             break;
+//         }
+//         else{
+//             j++;
+//         }
+//     }
+//     if(j==i){
+//         cout<<i<<" is prime number : "<< i <<endl;
+//     }
+//   }
+//  }
+// }
+
+// int main(){
+//     int s, e;
+   
+//     cin >> s >> e;
+
+//     primeNumber(s,e);
+//     return 0;
+// }
+
+
+
+//3.Check if a number is Palindrome.
+
 #include <iostream>
 #include <string>
 using namespace std; 
 
-void primeNumber(int s, int e ){
-    if(s<2){
-        s=2;
-    }
-    while(s<=e){
-        if(s %2 ==0 && s!=2){
-            s++;
-            continue;
-            cout<<"not a prime number :  "<<s<<endl;
-        }else{
-            cout << "prime number :  "<<s<<endl;
-            s++;
-        }
-    }
+bool CheckPalindrom(int n){
+
+  int originalNum = n;
+  int rever = 0;
+
+  while(n>0){
+
+    int rem = n%10;
+    rever = rever*10 + rem;
+    n=n/10;
+  }
+  return originalNum == rever;
 }
 
 int main(){
-    int s, e;
+    int n = 3443;
    
-    cin >> s >> e;
+if(CheckPalindrom(n))
+    cout<< n << " is a palindrome number"<<endl;
+else
+    cout<< n << " is not a palindrome number"<<endl;
 
-    primeNumber(s,e);
     return 0;
 }
+
